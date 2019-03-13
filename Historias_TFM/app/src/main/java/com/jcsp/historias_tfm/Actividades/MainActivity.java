8ff2277e1bdd6f555c9dcf1f4682d8ec28f5fef6
xenahort <juan.carlos.wow.95@gmail.com>
2019-03-13 09:49:20 +0100

    Codifica imagen de usuario a base64, contraseña cifrada con sha-1, acualizada versión de Android

--- Historias_TFM/app/src/main/java/com/jcsp/historias_tfm/Actividades/MainActivity.java
+++ Historias_TFM/app/src/main/java/com/jcsp/historias_tfm/Actividades/MainActivity.java
@@ -14,7 +14,7 @@ import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.jcsp.historias_tfm.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     private CallbackManager callbackManager;
     private LoginButton loginButton;
@@ -53,7 +53,7 @@ public class LoginActivity extends AppCompatActivity {
         btnRegistro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, RegistroActivity.class);
+                Intent intent = new Intent(MainActivity.this, RegistroActivity.class);
                 startActivityForResult(intent, 0);
             }
         });
