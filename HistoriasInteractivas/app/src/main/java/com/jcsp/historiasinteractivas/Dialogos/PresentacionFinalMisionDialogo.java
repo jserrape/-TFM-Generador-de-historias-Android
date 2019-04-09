e067a05617e6d7592ffc28192776699cbbd6b694
xenahort <juan.carlos.wow.95@gmail.com>
2019-04-09 14:38:29 +0200

    Dialogo de completar misión acabado

--- HistoriasInteractivas/app/src/main/java/com/jcsp/historiasinteractivas/Dialogos/PresentacionFinalMisionDialogo.java
+++ HistoriasInteractivas/app/src/main/java/com/jcsp/historiasinteractivas/Dialogos/PresentacionFinalMisionDialogo.java
@@ -12,7 +12,14 @@ package com.jcsp.historiasinteractivas.Dialogos;
 import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Base64;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.jcsp.historiasinteractivas.Objetos_gestion.Mision;
 import com.jcsp.historiasinteractivas.R;
@@ -32,6 +39,24 @@ public class PresentacionFinalMisionDialogo {
         this.mision = mis;
 
 
+        //Imagen
+        ImageView imgTit = (ImageView) dialogo.findViewById(R.id.imagen_final_historia);
+        byte[] imageAsBytes = Base64.decode(mision.getImagen_final().getBytes(), Base64.DEFAULT);
+        imgTit.setImageBitmap(BitmapFactory.decodeByteArray(imageAsBytes, 0, imageAsBytes.length));
+        imgTit.setAdjustViewBounds(true);
+
+        //Texto
+        TextView descripcion = (TextView) dialogo.findViewById(R.id.descripcion_final);
+        descripcion.setText(mis.getDescripcion_final().replaceAll("#", System.getProperty("line.separator") + System.getProperty("line.separator")));
+        descripcion.setMovementMethod(new ScrollingMovementMethod());
+
+        //Boton cerrar
+        Button btn_cerrar = (Button) dialogo.findViewById(R.id.cerrar_presenacion_final);
+        btn_cerrar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dialogo.dismiss();
+            }
+        });
 
         dialogo.show();
     }
